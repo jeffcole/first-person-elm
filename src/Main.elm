8f7b83c3ea1ab37cefa104a6c5b588aa577726c9
Jeff Cole <jeffreydonaldcole@gmail.com>
2019-07-04 08:50:03 -0400

    feat(scene): add webgl/first-person example
    Since we're using create-elm-app, we pass the bundled paths to the
    textures in to Elm as `flags`.
    
    https://github.com/halfzebra/create-elm-app/blob/master/template/README.md#adding-images-and-fonts

--- src/Main.elm
+++ src/Main.elm
@@ -1,57 +1,423 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Browser.Dom exposing (Viewport, getViewport)
+import Browser.Events exposing (onAnimationFrameDelta, onKeyDown, onKeyUp, onResize)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (height, style, width)
+import Html.Events exposing (keyCode)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline as DecodePipeline
+import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector2 as Vec2 exposing (Vec2, vec2)
+import Math.Vector3 as Vec3 exposing (Vec3, vec3)
+import Task exposing (Task)
+import WebGL exposing (Entity, Mesh, Shader)
+import WebGL.Texture as Texture exposing (Error, Texture)
 
 
 
----- MODEL ----
+---- MODEL
 
 
 type alias Model =
-    {}
+    { texture : Maybe Texture
+    , keys : Keys
+    , size : { width : Float, height : Float }
+    , person : Person
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
+type alias Person =
+    { position : Vec3
+    , velocity : Vec3
+    }
 
 
 type Msg
-    = NoOp
+    = TextureLoaded (Result Error Texture)
+    | KeyChange Bool Int
+    | Animate Float
+    | GetViewport Viewport
+    | Resize Int Int
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+type alias Keys =
+    { left : Bool
+    , right : Bool
+    , up : Bool
+    , down : Bool
+    , space : Bool
+    }
+
+
+type alias Flags =
+    { textures : Textures }
+
+
+type alias Textures =
+    { woodCratePath : String }
 
 
 
----- VIEW ----
+---- PROGRAM
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+main : Program Decode.Value Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , subscriptions = subscriptions
+        , update = update
+        }
+
+
+init : Decode.Value -> ( Model, Cmd Msg )
+init flagsValue =
+    let
+        flags =
+            decodeFlags flagsValue
+    in
+    ( { texture = Nothing
+      , person = Person (vec3 0 eyeLevel -10) (vec3 0 0 0)
+      , keys = Keys False False False False False
+      , size = { width = 0, height = 0 }
+      }
+    , Cmd.batch
+        [ Task.attempt TextureLoaded (Texture.load flags.textures.woodCratePath)
+        , Task.perform GetViewport getViewport
+        ]
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ onAnimationFrameDelta Animate
+        , onKeyDown (Decode.map (KeyChange True) keyCode)
+        , onKeyUp (Decode.map (KeyChange False) keyCode)
+        , onResize Resize
         ]
 
 
+eyeLevel : Float
+eyeLevel =
+    2
 
----- PROGRAM ----
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
+---- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update action model =
+    case action of
+        TextureLoaded textureResult ->
+            ( { model | texture = Result.toMaybe textureResult }, Cmd.none )
+
+        KeyChange on code ->
+            ( { model | keys = keyFunc on code model.keys }, Cmd.none )
+
+        GetViewport { viewport } ->
+            ( { model
+                | size =
+                    { width = viewport.width
+                    , height = viewport.height
+                    }
+              }
+            , Cmd.none
+            )
+
+        Resize width height ->
+            ( { model
+                | size =
+                    { width = toFloat width
+                    , height = toFloat height
+                    }
+              }
+            , Cmd.none
+            )
+
+        Animate dt ->
+            ( { model
+                | person =
+                    model.person
+                        |> move model.keys
+                        |> gravity (dt / 500)
+                        |> physics (dt / 500)
+              }
+            , Cmd.none
+            )
+
+
+keyFunc : Bool -> Int -> Keys -> Keys
+keyFunc on keyCode keys =
+    case keyCode of
+        32 ->
+            { keys | space = on }
+
+        37 ->
+            { keys | left = on }
+
+        39 ->
+            { keys | right = on }
+
+        38 ->
+            { keys | up = on }
+
+        40 ->
+            { keys | down = on }
+
+        _ ->
+            keys
+
+
+move : Keys -> Person -> Person
+move { left, right, up, down, space } person =
+    let
+        direction a b =
+            if a == b then
+                0
+
+            else if a then
+                1
+
+            else
+                -1
+
+        vy =
+            if space then
+                2
+
+            else
+                Vec3.getY person.velocity
+    in
+    if Vec3.getY person.position <= eyeLevel then
+        { person
+            | velocity =
+                vec3 (direction left right) vy (direction up down)
         }
+
+    else
+        person
+
+
+physics : Float -> Person -> Person
+physics dt person =
+    let
+        position =
+            Vec3.add person.position (Vec3.scale dt person.velocity)
+    in
+    { person
+        | position =
+            if Vec3.getY position < eyeLevel then
+                Vec3.setY eyeLevel position
+
+            else
+                position
+    }
+
+
+gravity : Float -> Person -> Person
+gravity dt person =
+    if Vec3.getY person.position > eyeLevel then
+        { person
+            | velocity =
+                Vec3.setY
+                    (Vec3.getY person.velocity - 2 * dt)
+                    person.velocity
+        }
+
+    else
+        person
+
+
+
+---- VIEW
+
+
+view : Model -> Html Msg
+view { size, person, texture } =
+    div
+        [ style "width" (String.fromFloat size.width ++ "px")
+        , style "height" (String.fromFloat size.height ++ "px")
+        , style "position" "absolute"
+        , style "left" "0"
+        , style "top" "0"
+        ]
+        [ WebGL.toHtmlWith
+            [ WebGL.depth 1
+            ]
+            [ width (round size.width)
+            , height (round size.height)
+            , style "display" "block"
+            ]
+            (texture
+                |> Maybe.map (scene size person)
+                |> Maybe.withDefault []
+            )
+        , div
+            [ style "position" "absolute"
+            , style "font-family" "monospace"
+            , style "color" "white"
+            , style "text-align" "center"
+            , style "left" "20px"
+            , style "right" "20px"
+            , style "top" "20px"
+            ]
+            [ text message ]
+        ]
+
+
+message : String
+message =
+    "Walk around with a first person perspective.\n"
+        ++ "Arrows keys to move, space bar to jump."
+
+
+scene : { width : Float, height : Float } -> Person -> Texture -> List Entity
+scene { width, height } person texture =
+    let
+        perspective =
+            Mat4.mul
+                (Mat4.makePerspective 45 (width / height) 0.01 100)
+                (Mat4.makeLookAt person.position (Vec3.add person.position Vec3.k) Vec3.j)
+    in
+    [ WebGL.entity
+        vertexShader
+        fragmentShader
+        crate
+        { texture = texture
+        , perspective = perspective
+        }
+    ]
+
+
+
+---- MESH
+
+
+type alias Vertex =
+    { position : Vec3
+    , coord : Vec2
+    }
+
+
+crate : Mesh Vertex
+crate =
+    [ ( 0, 0 ), ( 90, 0 ), ( 180, 0 ), ( 270, 0 ), ( 0, 90 ), ( 0, -90 ) ]
+        |> List.concatMap rotatedSquare
+        |> WebGL.triangles
+
+
+rotatedSquare : ( Float, Float ) -> List ( Vertex, Vertex, Vertex )
+rotatedSquare ( angleXZ, angleYZ ) =
+    let
+        transformMat =
+            Mat4.mul
+                (Mat4.makeRotate (degrees angleXZ) Vec3.j)
+                (Mat4.makeRotate (degrees angleYZ) Vec3.i)
+
+        transform vertex =
+            { vertex
+                | position =
+                    Mat4.transform transformMat vertex.position
+            }
+
+        transformTriangle ( a, b, c ) =
+            ( transform a, transform b, transform c )
+    in
+    List.map transformTriangle square
+
+
+square : List ( Vertex, Vertex, Vertex )
+square =
+    let
+        topLeft =
+            Vertex (vec3 -1 1 1) (vec2 0 1)
+
+        topRight =
+            Vertex (vec3 1 1 1) (vec2 1 1)
+
+        bottomLeft =
+            Vertex (vec3 -1 -1 1) (vec2 0 0)
+
+        bottomRight =
+            Vertex (vec3 1 -1 1) (vec2 1 0)
+    in
+    [ ( topLeft, topRight, bottomLeft )
+    , ( bottomLeft, topRight, bottomRight )
+    ]
+
+
+
+---- SHADERS
+
+
+type alias Uniforms =
+    { texture : Texture
+    , perspective : Mat4
+    }
+
+
+vertexShader : Shader Vertex Uniforms { vcoord : Vec2 }
+vertexShader =
+    [glsl|
+
+        attribute vec3 position;
+        attribute vec2 coord;
+        uniform mat4 perspective;
+        varying vec2 vcoord;
+
+        void main () {
+          gl_Position = perspective * vec4(position, 1.0);
+          vcoord = coord;
+        }
+
+    |]
+
+
+fragmentShader : Shader {} Uniforms { vcoord : Vec2 }
+fragmentShader =
+    [glsl|
+
+        precision mediump float;
+        uniform sampler2D texture;
+        varying vec2 vcoord;
+
+        void main () {
+          gl_FragColor = texture2D(texture, vcoord);
+        }
+
+    |]
+
+
+
+---- DECODERS
+
+
+decodeFlags : Decode.Value -> Flags
+decodeFlags flags =
+    let
+        flagsDecodeResult =
+            Decode.decodeValue flagsDecoder flags
+    in
+    Result.withDefault { textures = missingTextures } flagsDecodeResult
+
+
+flagsDecoder : Decoder Flags
+flagsDecoder =
+    Decode.succeed Flags
+        |> DecodePipeline.required "textures" texturesDecoder
+
+
+texturesDecoder : Decoder Textures
+texturesDecoder =
+    Decode.succeed Textures
+        |> DecodePipeline.required "woodCratePath" Decode.string
+
+
+missingTextures : Textures
+missingTextures =
+    { woodCratePath = "Missing" }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./main.css";
+import woodCratePath from "../textures/wood-crate.jpg";
 import { Elm } from "./Main.elm";
 import registerServiceWorker from "./registerServiceWorker";
 
 Elm.Main.init({
-  node: document.getElementById("root")
+  node: document.getElementById("root"),
+  flags: { textures: { woodCratePath } }
 });
 
 registerServiceWorker();
